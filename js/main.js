ce085b83b3444e8cf676a16efa350421001c6c64
coopfeathy <cooperfeatherstone13@gmail.com>
2022-11-05 18:33:32 -0600

    week 8 update

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@ const links = [
   "<li>Week 5: <a href='week5/groupwork.html'>Group Assignment</a></li>",
   "<hr><li>Week 6: <a href='week6/challenge.html'>Challenge One: To Do</a></li>",
   "<hr><li>Week 7: <a href='week7/notes.html'>Notes</a></li>",
-  "<li>Week 7: <a href='week7/groupwork.html'>Group Assignment: Hiking</a></li>"
+  "<li>Week 7: <a href='week7/groupwork.html'>Group Assignment: Hiking</a></li>",
+  "<hr><li>Week 8: <a href='week8/notes.html'>Notes</a></li>",
+  "<li>Week 8: <a href='week8/swapi.html'>Group Assignment: Star Wars API</a></li>"
 
 ];
 document.getElementById("toclist").innerHTML = links.join("");
